05f18cab45a587989e7fffa4935c6652e7dad46a
Anna Hadjitofi <annahadji@users.noreply.github.com>
2019-10-10 16:03:34 +0100

    Add loading and formating of crime data csv

--- resources/utils.py
+++ resources/utils.py
@@ -1,4 +1,23 @@
+import datetime
+import dateutil.rrule
+
 
 def check_import():
     print("Imported!")
     return 0
+
+def rename_category_for_flattening(category, category_parent):
+    """
+    #TODO: documentation
+    """
+    return category_parent + "_" + category.lower().replace(" ", "_")
+
+def sequential_months(dates):
+    """
+    #TODO: documentation
+    """
+    strt_dt = datetime.datetime.strptime(min(dates), "%Y%m")
+    end_dt = datetime.datetime.strptime(max(dates), "%Y%m")
+
+    n_months = len([dt for dt in dateutil.rrule.rrule(dateutil.rrule.MONTHLY, dtstart=strt_dt, until=end_dt)])
+    return n_months == len(dates)

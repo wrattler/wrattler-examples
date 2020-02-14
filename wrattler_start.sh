b6dddf52e8e6ded6b4efa54d1342bdee466b8e0a
Nick Barlow <nbarlow@turing.ac.uk>
2020-02-14 09:15:56 +0000

    permissions for start script, and don't start jupyterlab in start script

--- wrattler_start.sh
+++ wrattler_start.sh
@@ -6,4 +6,4 @@ cd server/R; Rscript app.R &
 cd -
 cd client; http-server public -c-1 --cors=http://localhost:8889 &
 cd -
-cd jupyterlab; jupyter lab --ip 0.0.0.0 --port 8889 --allow-root
+#cd jupyterlab; jupyter lab --ip 0.0.0.0 --port 8889 --allow-root

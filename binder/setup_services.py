c34e3574cf6904479861130317b5187e3adf9aa1
Nick Barlow <nbarlow@turing.ac.uk>
2020-02-17 13:13:25 +0000

    try to add AI assistants

--- binder/setup_services.py
+++ binder/setup_services.py
@@ -1,3 +1,10 @@
+"""
+Functions to start Wrattler services, to be used in entrypoints
+that jupyter-server-proxy can use, such that the services are
+available via <base_url>/proxy/port/
+"""
+
+
 def setup_pythonservice():
   return {
     'command': ['wrattler-python-service']
@@ -11,11 +18,17 @@ def setup_datastore():
 
 def setup_rservice():
   return {
-    'command': ['Rscript','wrattler/server/R/app.R']
+    'command': ['Rscript','/wrattler/server/R/app.R']
   }
 
 
 def setup_client():
   return {
-    'command': ['http-server','wrattler/client/public','-c-1','--cors']
+    'command': ['http-server','/wrattler/client/public','-c-1','--cors']
+  }
+
+
+def setup_aiassistants():
+  return {
+    'command': ['dotnet','/wrattler/aiassistants/server/bin/Debug/netcoreapp2.1/aiassistants.dll']
   }
